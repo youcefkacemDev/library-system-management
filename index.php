2c7b8624b9979aad080ebd442582fa98b66d750d
youcefkacemDev <youcefkacmegithub1998@gmail>
2023-12-21 21:25:23 +0100

    add style for the index page and rename the images

--- index.php
+++ index.php
@@ -1,29 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>library login</title>
+    <link rel="stylesheet" href="styles/index.style.css">
 </head>
 <body>
-    <div>
+    <div class="center">
+        <h1>Welcome To Librarian</h1>
+    </div>
+    <div class="center">
+        <h1>Log in here</h1>
+    </div>
+    <div class="form">
         <form action="" method="post">
             <div>
-                <label for="username">UserName :</label>
+                <label class="input-name" for="username">UserName :</label>
                 <div>
-                    <input type="text" name="username" id="username">
+                    <input type="text" name="username" id="username" placeholder="UserName" class="input-text">
                 </div>
             </div>
             <div>
-                <label for="password">Password :</label>
+                <label class="input-name" for="password">Password :</label>
                 <div>
-                    <input type="password" name="password" id="password">
+                    <input type="password" name="password" id="password" placeholder="Password" class="input-text">
                 </div>
             </div>
             <div>
-                <input type="submit" value="Log in">
+                <input type="submit" value="Log in" class="log-in">
             </div>
         </form>
     </div>
 </body>
+
 </html>
