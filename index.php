9cfc823d3ff7a81940215e1aef0692da8407d23d
youcefkacemDev <youcefkacmegithub1998@gmail>
2023-12-21 18:48:58 +0100

    add images to the images folder

--- index.php
+++ index.php
@@ -6,6 +6,24 @@
     <title>library login</title>
 </head>
 <body>
-    
+    <div>
+        <form action="" method="post">
+            <div>
+                <label for="username">UserName :</label>
+                <div>
+                    <input type="text" name="username" id="username">
+                </div>
+            </div>
+            <div>
+                <label for="password">Password :</label>
+                <div>
+                    <input type="password" name="password" id="password">
+                </div>
+            </div>
+            <div>
+                <input type="submit" value="Log in">
+            </div>
+        </form>
+    </div>
 </body>
 </html>
