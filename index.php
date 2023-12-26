1e8436bb2df43d40bc9886d8c2e16c7cc67abb39
iamjuliette <juliettemiso@gmail.com>
2023-12-26 12:58:34 +0100

    add style to the index page

--- index.php
+++ index.php
@@ -1,37 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>L.S.M</title>
+    <title>library login</title>
     <link rel="stylesheet" href="styles/index.style.css">
     <link rel="stylesheet" href="styles/animation.css">
 </head>
 <body>
-    <div class="center">
-        <h1>Welcome To Librarian</h1>
-    </div>
-    <div class="form">
-        <div class="form-title">
-            <h1>Log in here</h1>
-        </div>
+<header>
+<div class="header-logo">
+    <img src="images/i6.png">
+</div>
+<div class="header-title">
+ <h1>Library System Managment </h1>
+</div>
+</header>
+<div class="wrapper">
+    <div class="form-box login">
+        <h2>Login </h2>
         <form action="" method="post">
-            <div>
-                <label class="input-name" for="username">UserName :</label>
-                <div>
-                    <input type="text" name="username" id="username" placeholder="UserName" class="input-text">
-                </div>
-            </div>
-            <div>
-                <label class="input-name" for="password">Password :</label>
-                <div>
-                    <input type="password" name="password" id="password" placeholder="Password" class="input-text">
-                </div>
-            </div>
-            <div>
-                <input type="submit" value="Log in" class="log-in">
-            </div>
-        </form>
-    </div>
+            <div class="input-box">
+                <span class="icon">
+                    <ion-icon name="person-outline"></ion-icon>
+                </span>
+                <input type="text" name="username" id="username" required>
+                <label>UserName</label>
+             </div>
+             <div class="input-box">
+                <span class="icon">
+                    <ion-icon name="lock-closed-outline"></ion-icon>
+                </span>
+                <input type="password" name="password"  required>
+                <label>Password</label>
+             </div>
+            <button type="submit" class="log-in">Login
+            </button>
+        </form> 
+   </div>
+</div>
+
+<script src="script.js"></script>
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
